013cdc55550aa01bf66e15a6ca693de5b417f741
guiling-9 <2573167176@qq.com>
2025-10-21 16:12:05 +0800

    Implement basic calculator in C

---
+++ 3_.main.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+int main() {
+    int a, b;
+    char op;
+    
+
+    scanf("%d %d %c", &a, &b, &op);
+    
+
+    switch (op) {
+        case '+':
+            printf("%d\n", a + b);
+            break;
+        case '-':
+            printf("%d\n", a - b);
+            break;
+        case '*':
+            printf("%d\n", a * b);
+            break;
+        case '/':
+        
+            if (b != 0) {
+                printf("%d\n", a / b);
+            } else {
+                printf("Divisor cannot be zero\n");
+            }
+            break;
+        default:
+            printf("Invalid operator\n");
+    }
+    
+    return 0;
+}
