a0de9922de92e8a34b99ece3c38d4da3dbb25a97
guiling-9 <2573167176@qq.com>
2025-10-21 16:05:14 +0800

    Implement Celsius to Fahrenheit conversion
    Refactored main function to convert Celsius to Fahrenheit.

--- 1_main.c
+++ 1_main.c
@@ -1,9 +1,17 @@
-//
-//
-//
+//202510306217
+//2573167176qq.com
+//李昊
 #include <stdio.h>
-int main()
-{
-    printf("Hello World");
+
+int main() {
+    float celsius, fahrenheit;
+    
+    scanf("%f", &celsius);
+    
+
+    fahrenheit = celsius * 9 / 5 + 32;
+    
+    printf("%.1f\n", fahrenheit);
+    
     return 0;
 }
